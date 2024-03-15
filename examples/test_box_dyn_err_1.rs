ee738e93e51ce03b6458631afe9d9e945b918d76
Mathias <stadler-mathias@web.de>
2024-03-15 11:03:36 +0100

    in function

--- examples/test_box_dyn_err_1.rs
+++ examples/test_box_dyn_err_1.rs
@@ -28,23 +28,62 @@ fn foo_result_err() -> Result<String, Box<dyn Error>> {
 }
 
 fn main() {
-
     #[allow(unused_variables)]
     let foo_result_ok: Result<String, Box<dyn Error>> = foo_result_ok();
-    
+
     #[allow(unused_variables)]
-    let result: ()= match foo_result_ok {
-        Ok(result ) => println!("Ok => {:?}" ,result ),
+    let result: () = match foo_result_ok {
+        Ok(result) => println!("Ok => {:?}", result),
         Err(error) => panic!("Err => {:?}", error),
     };
-
 
     #[allow(unused_variables)]
     let foo_result_err: Result<String, Box<dyn Error>> = foo_result_err();
-    
+
     #[allow(unused_variables)]
-    let result: ()= match foo_result_err {
-        Ok(result ) => println!(" {:?}" ,result ),
+    let result: () = match foo_result_err {
+        Ok(result) => println!(" {:?}", result),
         Err(error) => panic!("Err => {:?}", error),
     };
 }
+
+// test
+
+#[cfg(test)]
+mod tests {
+
+    use super::*;
+
+    #[test]
+    fn result_test_ok() -> Result<(), Box<dyn Error>> {
+        #[allow(unused_variables)]
+        let foo_result_ok: Result<String, Box<dyn Error>> = foo_result_ok();
+
+        #[allow(unused_variables)]
+        let result: () = match foo_result_ok {
+            Ok(result) => println!("Ok => {:?}", result),
+            Err(error) => panic!("Err => {:?}", error),
+        };
+
+        Ok(result)
+    }
+
+    #[test]
+    fn result_test_err() -> Result<(), Box<dyn Error>> {
+
+        #[allow(unused_variables)]
+        let foo_result_err: Result<String, Box<dyn Error>> = foo_result_err();
+    
+        #[allow(unused_variables)]
+        let result: () = match foo_result_err {
+            Ok(result) => println!(" {:?}", result),
+            Err(error) => panic!("Err => {:?}", error),
+        };
+
+        Ok(result)
+
+    }
+}
+
+// cargo run --example test_box_dyn_err_1
+// cargo test --example test_box_dyn_err_1
