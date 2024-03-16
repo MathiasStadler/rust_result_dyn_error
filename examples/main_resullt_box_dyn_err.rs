3460c09e0bb11c41cd211913b0bab74607e9f58d
Mathias <stadler-mathias@web.de>
2024-03-16 09:34:20 +0100

    main_resullt_box_dyn_err works

--- examples/main_resullt_box_dyn_err.rs
+++ examples/main_resullt_box_dyn_err.rs
@@ -1,6 +1,7 @@
 // FROM HERE
 // https://doc.rust-lang.org/rust-by-example/error/multiple_error_types/boxing_errors.html
 
+// https://tourofrust.com/99_en.html
 
 mod my_env_logger;
 
@@ -15,9 +16,37 @@ use my_env_logger::*;
 
 use std::error::Error;
 
+// fn main() -> Result<(), Box<dyn Error>> {
+//     let result: Result<(), Box<dyn Error>> = Ok(());
+//     result?;
+//     Ok(())
+// }
+
+use std::fmt::Display;
+// use std::error::Error;
+
+struct Pie;
+
+#[derive(Debug)]
+struct NotFreshError;
+
+impl Display for NotFreshError {
+    fn fmt(&self, f: &mut std::fmt::Formatter<'_>) -> std::fmt::Result {
+        write!(f, "This pie is not fresh!")
+    }
+}
+
+impl Error for NotFreshError {}
+
+impl Pie {
+    fn eat(&self) -> Result<(), Box<dyn Error>> {
+        Err(Box::new(NotFreshError))
+    }
+}
+
 fn main() -> Result<(), Box<dyn Error>> {
-    let result: Result<(), Box<dyn Error>> = Ok(());
-    result?;
+    let heap_pie = Box::new(Pie);
+    heap_pie.eat()?;
     Ok(())
 }
 
